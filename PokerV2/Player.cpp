93f794d054294093731522eeb5ff9fab43b30ff0
JustinMorritt <Jayem_@hotmail.com>
2014-10-01 22:10:37 -0400

    wow need to fix!

--- PokerV2/Player.cpp
+++ PokerV2/Player.cpp
@@ -184,7 +184,7 @@ vector<CUE> OmahaPlayer::BuildCUEs()
 					CUE c;
 					for (int i = 0; i < CUEBuilder.size(); ++i)
 					{
-						if (i != pointer1 && i != pointer2)
+						if (i != pointer1 && i != pointer2)//WOW NEED TO FIX THIS NOT TAKING IN CONSIDERATION OTHER POINTERS...
 						{
 							c.push_back(CUEBuilder[i]);
 						}
